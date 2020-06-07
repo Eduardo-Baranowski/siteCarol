12472bf182cd7bd19fec06b95aa64111ffc12473
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2020-06-06 21:51:30 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+
+    return response.json({ok: true})
+});
+
+app.listen(process.env.APP_PORT, () => {
+    console.log('Server up and running ');
+})
