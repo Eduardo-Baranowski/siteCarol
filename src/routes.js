fe5b7b7930c57ce32e553e612f48e04a0cb231a5
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2020-06-09 13:08:36 -0300

    configurando errorTratamento

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,18 @@ const userController = require('./controllers/UserController');
 
 const routes = Router();
 
+const get = async () => {
+    return Promise.reject('Oops!').catch(err => {
+      throw new Error(err);
+    });
+  };
+  
+  get()
+    .then(console.log)
+    .catch(function(e) {
+      console.log(e);
+    });
+
 routes.get('/', async (request, response) => {
 
     //const user = await User.create({email: 'Teste', password: '123'});
@@ -22,6 +34,7 @@ routes.get('/', async (request, response) => {
 });
 
 routes.post('/criar-usuario', userController.create)
+
 
 
 module.exports = routes;
