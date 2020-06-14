42797988530f46ff03325aca940cf988bc1ade83
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2020-06-14 10:10:44 -0300

    configurando cartela

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,16 +1,31 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 module.exports = {
-    async create(request, response) {
-        const {email, password} = request.body;
-        console.log(request.body);
-        console.log(email, password);
+  async create(request, response) {
+    const { email, password } = request.body;
+    console.log(request.body);
+    console.log(email, password);
 
-        // {email: '...', password: '...'}
-        const user = await User.create({email: email, password: password});
+    // {email: '...', password: '...'}
+    const user = await User.create({ email: email, password: password });
 
-        console.log(user);
+    console.log(user);
 
-        return response.render('createUser.html', {user: user})
+    return response.render("createUser.html", { user: user });
+  },
+
+  /*
+    async show() {
+         buscar todos os button
+        const buttons = Buttons.findAll();
+        
+         {
+          number_id: number,
+         required: boolean
+         }
+         
+        return render('index.html', {buttons=buttons})
+
     }
-}
+    */
+};
