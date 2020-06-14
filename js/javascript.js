19c1bbcf96b83ded4b29d77e9279ed9e1fa476ac
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2020-06-14 14:14:22 -0300

    configurando cartelacomformComjs

---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+function sucesso() {
+  alert(
+    "Dados cadastrado com sucesso! Encaminharemos sua cartela ao seu email quando o pagamento for efetivado!"
+  );
+}
--- src/app.js
+++ src/app.js
@@ -1,31 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const nunjucks = require('nunjucks');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const nunjucks = require("nunjucks");
+const bodyParser = require("body-parser");
 //const handlebars = require('express-handlebars');
-const path = require('path');
-const routes = require('./routes');
-
+const path = require("path");
+const routes = require("./routes");
 
 const app = express();
 app.use(express.json({}));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/static', express.static(path.resolve(__dirname, '..', 'static')));
+app.use("/static", express.static(path.resolve(__dirname, "..", "static")));
+app.use("/js", express.static("js"));
 app.use(routes);
 
-nunjucks.configure(path.resolve(__dirname, 'views'), {
+nunjucks.configure(path.resolve(__dirname, "views"), {
   autoescape: true,
-  express: app
+  express: app,
 });
 
-
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(process.env.PORT, () => {
-    console.log('Server up and running ');
-})
+  console.log("Server up and running ");
+});
